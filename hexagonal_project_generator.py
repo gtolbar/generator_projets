dac21e505dfb534e0d69d50e360691cdf3882267
Gustavo Toledo <toledo970501@gmail.com>
2025-02-28 18:59:58 -0500

    Se agrega up lower case

--- hexagonal_project_generator.py
+++ hexagonal_project_generator.py
@@ -15,6 +15,11 @@ print("             ARCH - HEXAGONAL PROJECT GENERATOR    ")
 print("\n")
 
 
+def capitalizar_modelo(modelo):
+    """ Capitaliza la primera letra del modelo si no está en mayúsculas """
+    return modelo[:1].upper() + modelo[1:]
+
+
 def crear_estructura(proyecto, modelos):
     base_path = os.path.join(os.getcwd(), proyecto)
     
@@ -45,8 +50,6 @@ def crear_estructura(proyecto, modelos):
 
     # Archivos base (corregida la ruta)
     archivos_base = {
-        os.path.join(base_path, f"Ms{proyecto.capitalize()}Application.java"): 
-            "public class MsApplication { public static void main(String[] args) {} }",
         os.path.join(base_path, "infraestructure/configuration/BeanConfiguration.java"): 
             "public class BeanConfiguration {}",
         os.path.join(base_path, "infraestructure/exception/NoDataFoundException.java"): 
@@ -61,22 +64,23 @@ def crear_estructura(proyecto, modelos):
 
     # Crear archivos para cada modelo
     for modelo in modelos:
+        modelo_capitalizado = capitalizar_modelo(modelo)  # Asegurar que la primera letra sea mayúscula
         archivos_modelo = {
-            os.path.join(base_path, f"application/dto/{modelo}Request.java"): f"public class {modelo}Request {{}}",
-            os.path.join(base_path, f"application/dto/{modelo}Response.java"): f"public class {modelo}Response {{}}",
-            os.path.join(base_path, f"application/handler/{modelo}Handler.java"): f"public class {modelo}Handler {{}}",
-            os.path.join(base_path, f"application/handler/I{modelo}Handler.java"): f"public interface I{modelo}Handler {{}}",
-            os.path.join(base_path, f"application/mapper/{modelo}RequestMapper.java"): f"public class {modelo}RequestMapper {{}}",
-            os.path.join(base_path, f"application/mapper/{modelo}ResponseMapper.java"): f"public class {modelo}ResponseMapper {{}}",
-            os.path.join(base_path, f"domain/api/I{modelo}ServicePort.java"): f"public interface I{modelo}ServicePort {{}}",
-            os.path.join(base_path, f"domain/model/{modelo}.java"): f"public class {modelo} {{}}",
-            os.path.join(base_path, f"domain/spi/I{modelo}PersistencePort.java"): f"public interface I{modelo}PersistencePort {{}}",
-            os.path.join(base_path, f"domain/usecase/{modelo}UseCase.java"): f"public class {modelo}UseCase {{}}",
-            os.path.join(base_path, f"infraestructure/input/rest/{modelo}Controller.java"): f"public class {modelo}Controller {{}}",
-            os.path.join(base_path, f"infraestructure/output/jpa/adapter/{modelo}JpaAdapter.java"): f"public class {modelo}JpaAdapter {{}}",
-            os.path.join(base_path, f"infraestructure/output/jpa/entity/{modelo}Entity.java"): f"public class {modelo}Entity {{}}",
-            os.path.join(base_path, f"infraestructure/output/jpa/mapper/{modelo}EntityMapper.java"): f"public class {modelo}EntityMapper {{}}",
-            os.path.join(base_path, f"infraestructure/output/jpa/repository/I{modelo}Repository.java"): f"public interface I{modelo}Repository {{}}"
+            os.path.join(base_path, f"application/dto/{modelo_capitalizado}Request.java"): f"public class {modelo_capitalizado}Request {{}}",
+            os.path.join(base_path, f"application/dto/{modelo_capitalizado}Response.java"): f"public class {modelo_capitalizado}Response {{}}",
+            os.path.join(base_path, f"application/handler/{modelo_capitalizado}Handler.java"): f"public class {modelo_capitalizado}Handler {{}}",
+            os.path.join(base_path, f"application/handler/I{modelo_capitalizado}Handler.java"): f"public interface I{modelo_capitalizado}Handler {{}}",
+            os.path.join(base_path, f"application/mapper/{modelo_capitalizado}RequestMapper.java"): f"public class {modelo_capitalizado}RequestMapper {{}}",
+            os.path.join(base_path, f"application/mapper/{modelo_capitalizado}ResponseMapper.java"): f"public class {modelo_capitalizado}ResponseMapper {{}}",
+            os.path.join(base_path, f"domain/api/I{modelo_capitalizado}ServicePort.java"): f"public interface I{modelo_capitalizado}ServicePort {{}}",
+            os.path.join(base_path, f"domain/model/{modelo_capitalizado}.java"): f"public class {modelo_capitalizado} {{}}",
+            os.path.join(base_path, f"domain/spi/I{modelo_capitalizado}PersistencePort.java"): f"public interface I{modelo_capitalizado}PersistencePort {{}}",
+            os.path.join(base_path, f"domain/usecase/{modelo_capitalizado}UseCase.java"): f"public class {modelo_capitalizado}UseCase {{}}",
+            os.path.join(base_path, f"infraestructure/input/rest/{modelo_capitalizado}Controller.java"): f"public class {modelo_capitalizado}Controller {{}}",
+            os.path.join(base_path, f"infraestructure/output/jpa/adapter/{modelo_capitalizado}JpaAdapter.java"): f"public class {modelo_capitalizado}JpaAdapter {{}}",
+            os.path.join(base_path, f"infraestructure/output/jpa/entity/{modelo_capitalizado}Entity.java"): f"public class {modelo_capitalizado}Entity {{}}",
+            os.path.join(base_path, f"infraestructure/output/jpa/mapper/{modelo_capitalizado}EntityMapper.java"): f"public class {modelo_capitalizado}EntityMapper {{}}",
+            os.path.join(base_path, f"infraestructure/output/jpa/repository/I{modelo_capitalizado}Repository.java"): f"public interface I{modelo_capitalizado}Repository {{}}"
         }
 
         for ruta, contenido in archivos_modelo.items():
